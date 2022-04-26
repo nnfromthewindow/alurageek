ed4064b2f922b2415f6860b8757dd7a6d9057472
Nico <nuccelli@hotmail.com>
2022-04-26 19:56:47 -0300

    Trabajando en agregar producto

--- assets/js/dropImage.js
+++ assets/js/dropImage.js
@@ -2,33 +2,35 @@ const cuadro = document.querySelector("[data-cuadro]");
 
 cuadro.addEventListener("dragover", (e) => {
   e.preventDefault();
-  console.log("over");
 });
 cuadro.addEventListener("dragleave", (e) => {
   e.preventDefault();
-  console.log("leave");
 });
-
-const processFile = (file) => {
-  const docType = file.type;
-  const validExtensions = ["image/jpeg", "image/jpg", "image/png"];
-
-  const fileReader = new FileReader();
-  const id = `image-${Math.random().toString(32).substring(7)}`;
-  console.log(`id= ${id}`);
-
-  fileReader.addEventListener("load", (e) => {
-    const fileUrl = fileReader.result;
-    console.log(fileUrl);
-  });
-};
 
 cuadro.addEventListener("drop", (e) => {
   e.preventDefault();
-  const file = e.dataTransfer.files;
-  console.log(file);
-  console.log(processFile(file));
+  const file = e.dataTransfer.files[0];
+  //console.log(file.type);
+
   processFile(file);
 
   //console.log(e);
 });
+const processFile = (file) => {
+  const docType = file.type;
+  const validExtensions = ["image/jpeg", "image/jpg", "image/png"];
+  if (validExtensions.includes(docType)) {
+    const fileReader = new FileReader();
+    const id = `image-${Math.random().toString(32).substring(7)}`;
+    //console.log(`id= ${id}`);
+
+    fileReader.addEventListener("load", (e) => {
+      const fileUrl = fileReader.result;
+      console.log(fileUrl);
+    });
+
+    fileReader.readAsDataURL(file);
+  } else {
+    alert("Hubo un error");
+  }
+};
